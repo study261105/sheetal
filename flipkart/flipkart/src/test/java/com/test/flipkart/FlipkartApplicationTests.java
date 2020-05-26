e3147e71d0a81a19f18a47f35628fb98103e01d4
sheetal <sheetalsharma261105@gmail.com>
2020-05-27 01:08:46 +0800

    SDvvsd

---
+++ flipkart/flipkart/src/test/java/com/test/flipkart/FlipkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.flipkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
